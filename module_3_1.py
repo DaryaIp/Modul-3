de4f41b8fe5ef14276029903b2611b1bee3c9155
DaryaIp <ipatovadasha@bk.ru>
2024-06-27 17:08:43 +0300

    Add files via upload

--- module_3_1.py
+++ module_3_1.py
@@ -2,19 +2,22 @@ calls = 0
 def  count_calls():
     global calls
     calls += 1
-    print(f"{calls}")
+    return calls
 
 def string_info(string):
+    count_calls()
     upper_ = string.upper()
     lower_ = string.lower()
     return len(string), upper_, lower_
 
 
 def is_contains(string, list_to_search):
+    count_calls()
     for string_ in list_to_search:
         if string_ == string:
             return True
     return False
+
 
 
 print(string_info('Хозяйство'))
